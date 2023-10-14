662378b4c9575179c35ffc2a29e8e77d7d6f0457
aloks <aloks@Dell>
2023-10-14 21:10:03 +0530

    First Commit - Application Base structure commited.

---
+++ Spring-Batch-UploadCSV/src/main/java/com/simplesolutions/SpringBatchUploadCsvApplication.java
@@ -0,0 +1,13 @@
+package com.simplesolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchUploadCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchUploadCsvApplication.class, args);
+	}
+
+}
